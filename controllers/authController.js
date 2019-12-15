f85dd6327e690de8e710d167b69cb8fb28c84e53
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2019-12-15 22:34:13 +0100

    add reset pasword feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,16 +7,17 @@ exports.signIn = async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     let user = await User.findOne({ email: req.body.email });
-    if (!user.active)
-        return res.status(400).send("User is not confirm. Check your email.");
     if (!user) return res.status(400).send("Invalid email or password");
     const validPassword = await bcrypt.compare(
         req.body.password,
         user.password
     );
-
     if (!validPassword)
         return res.status(400).send("Invalid email or password");
+
+    if (!user.active)
+        return res.status(400).send("User is not confirm. Check your email.");
+
     const token = user.generateAuthToken();
     res.send(token);
 };
@@ -30,7 +31,7 @@ function validate(req) {
         password: Joi.string()
             .min(5)
             .max(1024)
-            .required(),
+            .required()
     };
     return Joi.validate(req, schema);
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,7 @@ exports.getAllUsers = (req, res, next) => {
             res.status(200).json({
                 message: "Fetched users successfully.",
                 users: users,
-                userLength: users.length,
+                userLength: users.length
             });
         })
         .catch(err => {
@@ -54,7 +54,7 @@ exports.signUp = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const random = crypto.randomBytes(20).toString("hex");
     user.password = await bcrypt.hash(user.password, salt);
-    user.confirmationHash = random;
+    user.accountConfirmation = random;
 
     await user.save();
     const token = user.generateAuthToken();
@@ -66,14 +66,14 @@ exports.signUp = async (req, res) => {
         service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
+            pass: process.env.EMAIL_PASS
+        }
     });
     const mailOptions = {
         from: "Kanba",
         to: req.body.email,
         subject: "Kanba Welcome",
-        html: `Hello,<br> Please Click on the link to verify your email.<br><a href="http://localhost:3000/verify/${random}">Click here to verify</a>`,
+        html: `Hello,<br> Please Click on the link to verify your email.<br><a href="http://localhost:3000/verify/${random}">Click here to verify</a>`
     };
     transporter.sendMail(mailOptions, function(err, info) {
         if (err) console.log(err);
@@ -83,23 +83,73 @@ exports.signUp = async (req, res) => {
 
 exports.verify = async (req, res) => {
     const hash = req.params.hash;
-    const confirm = await User.findOne({ confirmationHash: hash });
-    if (!confirm) {
+    const user = await User.findOne({ accountConfirmation: hash });
+    if (!user) {
         return res.status(400).send({
             type: "bad-link",
-            message: "We were unable to find a user for this link.",
+            message: "We were unable to find a user for this link."
         });
     }
-    if (confirm.active)
+    if (user.active)
         return res.status(400).send({
             type: "already-verified",
-            message: "This user has already been verified.",
+            message: "This user has already been verified."
         });
 
-    confirm.active = true;
-    confirm.confirmationHash = "";
-    await confirm.save();
+    user.active = true;
+    user.accountConfirmation = "";
+    await user.save();
+    return res.status(201).json({
+        message: "User confirmation successfully."
+    });
+};
+exports.resetPassword = async (req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("User is not exist.");
+    if (!user.active) return res.status(400).send("User is not active.");
+    const random = crypto.randomBytes(20).toString("hex");
+    user.emailConfirmation = random;
+    await user.save();
+
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+    const mailOptions = {
+        from: "Kanba",
+        to: req.body.email,
+        subject: "Kanba - reset password request",
+        html: `Hello,<br> Please Click on the link to set a new password to your account.<br><a href="http://localhost:3000/set-new-password/${random}">Set new password</a>`
+    };
+    transporter.sendMail(mailOptions, function(err, info) {
+        if (err) console.log(err);
+        else console.log(info);
+    });
+    return res.status(201).json({
+        message: "Email was sended"
+    });
+};
+exports.setPassword = async (req, res) => {
+    const hash = req.body.hash;
+    const password = req.body.password;
+    const user = await User.findOne({ emailConfirmation: hash });
+    if (!user)
+        return res
+            .status(400)
+            .send("We were unable to find a user for this link.");
+    if (password.length < 5)
+        return res
+            .status(400)
+            .send("Password length must equal 5 character or be longer.");
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    user.emailConfirmation = "";
+    await user.save();
     return res.status(200).json({
-        message: "User confirmation successfully.",
+        message: "Password was changed successfuly"
     });
 };
