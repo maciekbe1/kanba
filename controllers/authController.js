7b791b7945612cf97cf31176c4553614dc1a09f2
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2019-12-29 22:16:35 +0100

    select active from me

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,23 +3,29 @@ import { User } from "../models/User";
 import _ from "lodash";
 import Joi from "joi";
 
-exports.signIn = async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send("Invalid email or password");
-    const validPassword = await bcrypt.compare(
-        req.body.password,
-        user.password
-    );
-    if (!validPassword)
-        return res.status(400).send("Invalid email or password");
+exports.signIn = async (req, res, next) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(400).send("Invalid email or password");
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword)
+            return res.status(400).send("Invalid email or password");
 
-    if (!user.active)
-        return res.status(400).send("User is not confirm. Check your email.");
+        if (!user.active)
+            return res
+                .status(400)
+                .send("User is not confirm. Check your email.");
 
-    const token = user.generateAuthToken();
-    res.send(token);
+        const token = user.generateAuthToken();
+        res.send(token);
+    } catch (err) {
+        next(err);
+    }
 };
 function validate(req) {
     const schema = {
