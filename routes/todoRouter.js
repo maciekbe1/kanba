9f2c298b271c4a90319fb23f05dec78afc75e034
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-01-06 21:13:50 +0100

    create todo list, update packages

---
+++ routes/todoRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import todoController from "../controllers/todoController";
+import auth from "../middleware/authMiddleware";
+
+const router = express.Router();
+
+router.post("/create-todo-list", auth, todoController.createTodoList);
+router.post("/add-todo-item", auth, todoController.addTodoItem);
+router.post("/get-user-todo-lists", auth, todoController.getUserTodoLists);
+router.delete("/remove-todo-list", auth, todoController.removeTodoList);
+
+module.exports = router;
