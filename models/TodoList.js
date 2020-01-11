c2afea988cfad1db24478715f39983d3c0276ee1
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-01-11 22:48:15 +0100

    add optional joi vaslidation - allow empty string

--- models/TodoList.js
+++ models/TodoList.js
@@ -2,40 +2,42 @@ import mongoose from "mongoose";
 import Joi from "joi";
 
 const todoSchema = new mongoose.Schema({
-    user: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        maxlength: 30,
-        required: true
-    },
-    description: {
-        type: String,
-        default: ""
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now()
-    },
-    list: {
-        type: Array,
-        default: []
-    }
+  user: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    maxlength: 30,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ""
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now()
+  },
+  list: {
+    type: Array,
+    default: []
+  }
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
 
 function validateTodo(todo) {
-    const schema = {
-        user: Joi.string().required(),
-        title: Joi.string()
-            .max(30)
-            .required(),
-        description: Joi.string().required()
-    };
-    return Joi.validate(todo, schema);
+  const schema = {
+    user: Joi.string().required(),
+    title: Joi.string()
+      .max(30)
+      .required(),
+    description: Joi.string()
+      .optional()
+      .allow("")
+  };
+  return Joi.validate(todo, schema);
 }
 exports.Todo = Todo;
 exports.validate = validateTodo;
