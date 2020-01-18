55934121f6d3bd70110e03bb86be0594cf903d7b
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-01-18 22:33:14 +0100

    fix todo path

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -4,9 +4,9 @@ import auth from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.post("/create-todo-list", auth, todoController.createTodoList);
+router.post("/create-todo-card", auth, todoController.createTodoCard);
 router.post("/add-todo-item", auth, todoController.addTodoItem);
-router.post("/get-user-todo-lists", auth, todoController.getUserTodoLists);
-router.post("/remove-todo-list", auth, todoController.removeTodoList);
+router.post("/get-user-todo-cards", auth, todoController.getUserTodo);
+router.post("/remove-todo-card", auth, todoController.removeTodoCard);
 
 module.exports = router;
