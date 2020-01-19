c61535ea4c42d7e9032099ecab4c6c011360dcf0
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-01-19 13:15:16 +0100

    add function remove todo item

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -8,5 +8,5 @@ router.post("/create-todo-card", auth, todoController.createTodoCard);
 router.post("/add-todo-item", auth, todoController.addTodoItem);
 router.post("/get-user-todo-cards", auth, todoController.getUserTodo);
 router.post("/remove-todo-card", auth, todoController.removeTodoCard);
-
+router.post("/remove-todo-item", auth, todoController.removeTodoItem);
 module.exports = router;
