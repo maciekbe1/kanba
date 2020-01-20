be34fdcdf26ab72b152e42b2f80c3717c84d6f49
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-01-20 23:46:30 +0100

    card update

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -9,4 +9,6 @@ router.post("/add-todo-item", auth, todoController.addTodoItem);
 router.post("/get-user-todo-cards", auth, todoController.getUserTodo);
 router.post("/remove-todo-card", auth, todoController.removeTodoCard);
 router.post("/remove-todo-item", auth, todoController.removeTodoItem);
+router.post("/update-todo-card", auth, todoController.updateTodoCard);
+
 module.exports = router;
