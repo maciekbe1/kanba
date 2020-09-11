5ae503557a8f49fd3ea6c97d7dd2b8221eab2607
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-09-12 00:30:55 +0200

    change google client id used to login via google

--- client/src/constants/user.js
+++ client/src/constants/user.js
@@ -1,3 +1,3 @@
 export const SESSION_MESSAGE = "Your session has expired, please login again.";
 export const GOOGLE_CLIENT =
-  "688306988829-lotg6jqsicjn1m11v02qa1l66jm67u9j.apps.googleusercontent.com";
+  "589695081750-v6801so15qlmspnr50cm2orv17g1tdk5.apps.googleusercontent.com";
